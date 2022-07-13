d3d1c7c96ff1128e34d4c9a2c1de6ce551975e4d
Mukesh <p.mukeshkannan@rifluxyss.com>
2022-07-13 11:31:48 +0530

    Android SDK

---
+++ CustomUtilites/src/main/java/com/example/customutilites/Utils.java
@@ -0,0 +1,12 @@
+package com.example.customutilites;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Utils {
+
+    public static void showToast(Context context, String message){
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+
+}
