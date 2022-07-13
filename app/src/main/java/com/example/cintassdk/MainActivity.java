2a58dd05621661d9c36497e1a4b6e9e424c5e458
Mukesh <p.mukeshkannan@rifluxyss.com>
2022-07-13 16:55:19 +0530

    Room Database - CINTAS MODEL

--- app/src/main/java/com/example/cintassdk/MainActivity.java
+++ app/src/main/java/com/example/cintassdk/MainActivity.java
@@ -2,9 +2,6 @@ package com.example.cintassdk;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import com.example.customutilites.Model;
-import com.example.customutilites.Utils;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     @Override
