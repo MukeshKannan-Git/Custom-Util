df64a0cd46ec03dbbf983528e1d770783e634a29
Mukesh <p.mukeshkannan@rifluxyss.com>
2022-07-13 17:36:08 +0530

    Bug Fix - getSystemDetails

--- CustomUtilites/src/main/java/com/example/customutilites/Utils.java
+++ CustomUtilites/src/main/java/com/example/customutilites/Utils.java
@@ -2,11 +2,16 @@ package com.example.customutilites;
 
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Build;
+import android.util.Log;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
-public class Utils extends Thread {
+public class Utils {
 
     public static void insertRoom(final Context context, final Model model) {
         RoomDB roomDB = RoomDB.getInstance(context.getApplicationContext());
@@ -22,5 +27,29 @@ public class Utils extends Thread {
         RoomDB roomDB = RoomDB.getInstance(context.getApplicationContext());
         return roomDB.personDao().getPersonList();
     }
-    
+
+    public static JSONObject getSystemDetails() {
+        try {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("SERIAL: ", Build.SERIAL);
+            jsonObject.put("MODEL: ", Build.MODEL);
+            jsonObject.put("ID: ", Build.ID);
+            jsonObject.put("Manufacture: ", Build.MANUFACTURER);
+            jsonObject.put("BRAND: ", Build.BRAND);
+            jsonObject.put("TYPE: ", Build.TYPE);
+            jsonObject.put("USER: ", Build.USER);
+            jsonObject.put("BASE: ", Build.VERSION_CODES.BASE);
+            jsonObject.put("INCREMENTAL ", Build.VERSION.INCREMENTAL);
+            jsonObject.put("SDK  ", Build.VERSION.SDK);
+            jsonObject.put("BOARD: ", Build.BOARD);
+            jsonObject.put("BRAND ", Build.BRAND);
+            jsonObject.put("HOST ", Build.HOST);
+            jsonObject.put("FINGERPRINT: ", Build.FINGERPRINT);
+            jsonObject.put("VERSION CODE: ", Build.VERSION.RELEASE);
+            return jsonObject;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return new JSONObject();
+    }
 }
