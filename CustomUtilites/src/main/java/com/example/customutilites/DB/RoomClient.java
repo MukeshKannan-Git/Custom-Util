2acc541a8c8496131f2699cda219961aced080f3
Mukesh <p.mukeshkannan@rifluxyss.com>
2022-07-14 15:27:25 +0530

    Room DataBase

--- CustomUtilites/src/main/java/com/example/customutilites/DB/RoomClient.java
+++ CustomUtilites/src/main/java/com/example/customutilites/DB/RoomClient.java
@@ -6,21 +6,21 @@ import java.util.List;
 
 public class RoomClient {
 
-    public static void insert(final Context context, final Model model) {
+    public static void insert(Context context, Model model) {
         if (context != null && model != null) {
             RoomDB roomDB = RoomDB.getInstance(context.getApplicationContext());
             roomDB.personDao().insertPerson(model);
         }
     }
 
-    public static void delete(final Context context, int userID) {
+    public static void delete(Context context, int userID) {
         if (context != null) {
             RoomDB roomDB = RoomDB.getInstance(context.getApplicationContext());
             roomDB.personDao().deletePerson(userID);
         }
     }
 
-    public static List<Model> get(final Context context) {
+    public static List<Model> get(Context context) {
         if (context != null) {
             RoomDB roomDB = RoomDB.getInstance(context.getApplicationContext());
             return roomDB.personDao().getPersonList();
